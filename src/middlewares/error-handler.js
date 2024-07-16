cf9125272844219f2bdfa813ff6aaae031f9473f
alnah <alexis.nahan@gmail.com>
2024-07-15 23:37:26 -0300

    feat(error-handler): add stack trace only if dev

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -3,6 +3,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   res.status(500).json({
     message: "An internal server error occurred",
     error: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 

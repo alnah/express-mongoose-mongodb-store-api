0e8c4e5896ce8986ee0aa4e5c1b1fa05677f1c07
alnah <alexis.nahan@gmail.com>
2024-07-17 14:10:41 -0300

    feat(products): add pagination to query

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -14,10 +14,13 @@ const getAllProducts = async (req, res, next) => {
     ? result.sort(sort.split(",").join(" "))
     : result.sort("createdAt");
 
-  result = fields
-    ? result.select(fields.split(",").join(" "))
-    : result;
-    
+  result = fields ? result.select(fields.split(",").join(" ")) : result;
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  
   const products = await result;
   res.status(200).json({ products, query });
 };

7cc6fd151876d35f18eedfae6530c866090d3f33
alnah <alexis.nahan@gmail.com>
2024-07-17 09:44:45 -0300

    feat(products): add sort to query

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,14 +1,19 @@
 const Product = require("../models/product");
 
 const getAllProducts = async (req, res, next) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const query = {};
 
   featured ? (query.featured = featured === "true") : false;
   company ? (query.company = company) : null;
   name ? (query.name = { $regex: name, $options: "i" }) : null;
 
-  const products = await Product.find(query);
+  let result = Product.find(query);
+  result = sort
+    ? result.sort(sort.split(",").join(" "))
+    : result.sort("createdAt");
+
+  const products = await result;
   res.status(200).json({ products, query });
 };
 
